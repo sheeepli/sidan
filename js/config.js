dc8fa7a5abe50053c46976d72574c0359894cd06
李舜阳 <2624791429@qq.com>
2022-10-19 10:07:54 +0800

    feat: finish

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-var serverUrl = 'http://chaiyv.cn/kh/ljy'
+var serverUrl = "http://chaiyv.cn/kh/ljy";
 
 function getParameter(el) {
   var obj = {};
@@ -9,3 +9,29 @@ function getParameter(el) {
   });
   return obj;
 }
+
+const ENUM = {
+  xsxm: "xsxm",
+  xsbh: "xsbh",
+  csjm: "csjm",
+  szzb: "szzb",
+  xsjg: "xsjg",
+  shjx: "shjx",
+  sfjj: "sfjj",
+  wcfs: "wcfs",
+  bxfs: "bxfs",
+  zfs: "zfs",
+};
+
+const ENUM_TEXT = {
+  [ENUM.xsxm]: "选手姓名",
+  [ENUM.xsbh]: "选手编号",
+  [ENUM.csjm]: "参赛节目",
+  [ENUM.szzb]: "所在组别",
+  [ENUM.xsjg]: "选送机构",
+  [ENUM.shjx]: "所获奖项",
+  [ENUM.sfjj]: "是否晋级",
+  [ENUM.wcfs]: "作品完成度分数",
+  [ENUM.bxfs]: "艺术表现力分数",
+  [ENUM.zfs]: "总分",
+};
